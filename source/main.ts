646e0240adbb92c4d024ad2ad33c0ab96530e802
clayton <me@clayton.cc>
2019-09-24 20:16:44 -0700

    Move source

--- source/main.ts
+++ source/main.ts
@@ -1,9 +1,10 @@
 import * as Discord from 'discord.js';
-import * as config from "./config.json";
+import * as Config from '../config.json';
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login(config.discord.api_key);
+client.login(Config.discord.api_key);
