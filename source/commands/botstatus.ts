7acc1c0f4fbd802a1a4c82de16961958619d5a54
clayton <me@clayton.cc>
2019-09-24 22:42:41 -0700

    Add command framework

---
+++ source/commands/botstatus.ts
@@ -0,0 +1,21 @@
+import { Command } from '../interfaces';
+
+function pluralize(text: string, value: number): string {
+    return value + ' ' + (value === 1 ? text : text + 's');
+}
+
+export default <Command> {
+    name: 'botstatus',
+    permission: 'NONE',
+    action: async (message) => {
+        const client = message.client;
+        const reply: Array<string> = [
+            `Hello, I'm ${client.user.username}, nice to meet you!`,
+            `I'm used by ${pluralize('user', client.users.size)} in ${pluralize('server', client.guilds.size)} and ${pluralize('channel', client.channels.size)}.`,
+            `My uptime is ${Math.round(client.uptime / 1000 / 60 / 60)} hours, ${Math.round(client.uptime / 1000 / 60) % 60} minutes, and ${(client.uptime / 1000) % 60} seconds.`,
+            `Memory usage: ${Math.round(process.memoryUsage().rss / 1024 / 1000)}MB`
+        ];
+
+        await message.channel.send(reply)
+    }
+}
---
+++ source/index.ts
@@ -0,0 +1,36 @@
+import * as Discord from 'discord.js';
+import { readdirSync } from 'fs';
+
+import * as config from '../config.json';
+import { Command } from './interfaces';
+
+const client = new Discord.Client();
+const commands: Array<Command> = [];
+
+for (const dirent of readdirSync(`${__dirname}/commands`, { withFileTypes: true }))
+    if (dirent.isFile() && dirent.name.endsWith('.js'))
+        commands.push(require(`./commands/${dirent.name}`).default);
+
+client.on('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    client.user.setPresence({
+        status: 'online',
+        activity: {
+            name: 'ShinyLan',
+            type: 'LISTENING'
+        }
+    });
+});
+
+client.on('message', message => {
+    if (message.content.startsWith(config.discord.command_prefix)) {
+        const withoutPrefix = message.content.substring(config.discord.command_prefix.length);
+
+        for (const command of commands)
+            if (withoutPrefix.startsWith(command.name + ' '))
+                command.action(message);
+    }
+});
+
+client.login(config.discord.api_key);
