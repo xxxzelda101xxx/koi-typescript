f17a073c9ef6d00d71f46d099c4a4eb0d9e3bfcd
clayton <me@clayton.cc>
2019-09-25 15:34:19 -0700

    Add Discord debugging

--- source/index.ts
+++ source/index.ts
@@ -7,6 +7,19 @@ import * as config from '../config.json';
 const client = new Discord.Client();
 const commands: Array<Command> = [];
 
+if (config.debug.enabled) {
+    const debugChannel = <Discord.TextChannel | Discord.DMChannel> client.channels.get(config.debug.channel_id);
+
+    console.log(debugChannel);
+
+    if (debugChannel !== undefined)
+        process.on('uncaughtException', async (error) => {
+            console.error(error);
+            await debugChannel.send(`Uncaught exception:\n${error}`);
+            process.exit(1);
+        });
+}
+
 for (const dirent of readdirSync(`${__dirname}/commands`, { withFileTypes: true }))
     if (dirent.isFile() && dirent.name.endsWith('.js'))
         commands.push(require(`./commands/${dirent.name}`).default);
